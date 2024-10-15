57abb9d610cb605a8ca597b26971a44437ab5edc
Christopher Kelvin <christopherkelvin100@gmail.com>
2024-10-15 19:21:13 +0300

    reduced code

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
 
-router.use("/add-product", (req, res, next) => {
+router.get("/add-product", (req, res, next) => {
   res.send(
     '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
   );
 });
-router.use("/product", (req, res, next) => {
+
+
+router.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use("/", (req, res, next) => {
-  console.log("In another middleware");
-  res.send("<h1>Home Cryser</h1>");
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes=require('./routes/shop')
+const app = express();
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+app.use((req, res, next) => { 
+  res.status(404).send('<h1>Page Not Found</h1>');
+   });
+
+
+
 app.listen(3000);
