45df609e7aa6a945df30134bc19f56e53b836f71
Christopher Kelvin <christopherkelvin100@gmail.com>
2024-10-15 09:50:30 +0300

    More clean code

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const http = require('http');
 
 const express = require('express');
 
 const app = express();
 app.use((req, res, next) => {
   console.log('In the middleware');
+  next();
 });
-const server = http.createServer(app);
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send('<h1>Hello Cryser</h1>');
+});
+app.listen(3000);
