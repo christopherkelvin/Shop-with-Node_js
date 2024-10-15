f30c2aceecfae0e378e684e1259a989fe85cd031
Christopher Kelvin <christopherkelvin100@gmail.com>
2024-10-15 16:05:07 +0300

    Express router

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next();
-});
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use("/", (req, res, next) => {
   console.log("In another middleware");
-  res.send('<h1>Hello Cryser</h1>');
+  res.send("<h1>Home Cryser</h1>");
 });
 app.listen(3000);
